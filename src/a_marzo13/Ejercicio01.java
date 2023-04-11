4b087295aa44b25a7f55ae23e92356cedc158a63
dacm1999 <danielcontreras12.dc@gmail.com>
2023-04-11 12:41:12 +0200

    EJERCICIOS ACCESO A DATOS - RECUPERACIÓN

--- src/a_marzo13/Ejercicio01.java
+++ src/a_marzo13/Ejercicio01.java
@@ -5,15 +5,18 @@ import java.io.File;
 public class Ejercicio01 {
 
     public static void main(String[] args) {
+        File f = new File(".");
 
-        File file = new File(".");
+        System.out.println("--------------RUTA ACTUAL--------------");
+        System.out.println(f.getAbsolutePath());
 
-        File[] archivos = file.listFiles();
+        System.out.println("");
 
-        for(int i = 0 ; i < archivos.length; i++ ){
-            System.out.println("Archivos en el directorio" + archivos[i]);
+        String archivos[] = f.list();
+        System.out.println("--------------ARCHIVOS--------------");
+        for (int i = 0; i < archivos.length; i++) {
+            System.out.println(archivos[i]);
         }
-
 
     }
 }
